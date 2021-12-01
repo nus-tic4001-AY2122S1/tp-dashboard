1b82b2acdc5f59800e913c9e3e581e1882be36bb
Damith C. Rajapakse <damith@gmail.com>
2021-12-01 16:34:34 +0800

    Stop cron job

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 ./get-reposense.py --master
-java -jar RepoSense.jar --config ./configs --since 17/09/2021  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+java -jar RepoSense.jar --config ./configs --until 19/11/2021 --since 17/09/2021  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
